03d80cfd45990bfd3a16b072065191b6e78349e1
SmellydogCoding <smellydogcoding@gmail.com>
2017-07-20 17:37:16 -0400

    more unit tests

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
 'use strict';
 
-const expect = require('chai').expect;
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
 const geospacial = require('./node/geospacial.js');
+const trailapi = require('./node/trailapi.js');
+const weatherapi = require('./node/weather.js');
+
+describe('testing test styles', () => {
+  it('should say that an object with an array of objects has a property of name', () => {
+    expect( {"places": [ {"city": "nemacolin"} ] } ).to.nested.include({"places[0]" : "city"});
+  });
+});
 
 // geospacial test suite
 
@@ -19,10 +31,49 @@ describe('test functions in the geospacial module', () => {
 
   // getGPSData function
 
+  let getTestCoords = geospacial.getGPSData('address=15351');
+
   it('should return an object when calling the getGPSData function', () => {
-    geospacial.getGPSData('address=15351').then((coords) => {
-      expect(coords).to.be.a('object');
-    });
+    return expect(getTestCoords).to.eventually.be.an('object');
+  });
+  it('should return an object with a coords key when calling the getGPSData function', () => {
+    return expect(getTestCoords).to.eventually.have.property('coords');
+  });
+  it('should have property lat and lng (which are numbers) on the coords key', () => {
+    expect(getTestCoords).to.eventually.have.property('coords').with.property('lat').to.be.a('number');
+    return expect(getTestCoords).to.eventually.have.property('coords').with.property('lng').to.be.a('number');
   });
 
+});
+
+// trail api test suite
+
+describe('test functions in the trailapi module', () => {
+
+  let getTestPlaces = trailapi.getPlaces({lat: '39.80001161', lng: '-80.22746854', activity: 'hiking', radius: '19'});
+
+  it('should return an object when calling the getplaces function', () => {
+    return expect(getTestPlaces).to.eventually.be.an('object');
+  });
+  it('should return an object with an array of objects called places', () => {
+    return expect(getTestPlaces).to.eventually.have.property('places').to.be.an('array').to.have.lengthOf.above(0);
+  });
+  it('should return an object with an array of objects with a name property', () => {
+    // return expect(getTestPlaces).to.eventually.nested.include({'getTestPlaces.places' : 'name'});
+  });
+
+});
+
+// openweather api test suite
+
+describe('test functions in the openweather api module', () => {
+  let getTestWeather = weatherapi.getWeather('39.80001161','-80.22746854');
+
+  it('should return an object when calling the getweather function', () => {
+    return expect(getTestWeather).to.eventually.be.an('object');
+  });
+  it('should return an object with a properties of main and wind', () => {
+    expect(getTestWeather).to.eventually.have.property('main');
+    return expect(getTestWeather).to.eventually.have.property('wind');
+  });
 });
