fe9dac9b42c133c191d242d6621631e257ee9fbf
SmellydogCoding <smellydogcoding@gmail.com>
2017-07-21 13:57:21 -0400

    tests,tests,tests

--- test.js
+++ test.js
@@ -4,14 +4,16 @@ const chai = require('chai');
 const expect = chai.expect;
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
+const httpMocks = require('node-mocks-http');
 
 const geospacial = require('./node/geospacial.js');
 const trailapi = require('./node/trailapi.js');
 const weatherapi = require('./node/weather.js');
+const mid = require('./node/middleware.js');
 
 describe('testing test styles', () => {
   it('should say that an object with an array of objects has a property of name', () => {
-    expect( {"places": [ {"city": "nemacolin"} ] } ).to.nested.include({"places[0]" : "city"});
+    expect( {"places": [ {"city": "nemacolin"} ] } ).to.have.nested.property("places[0].city");
   });
 });
 
@@ -50,6 +52,8 @@ describe('test functions in the geospacial module', () => {
 
 describe('test functions in the trailapi module', () => {
 
+  // getplaces function
+  
   let getTestPlaces = trailapi.getPlaces({lat: '39.80001161', lng: '-80.22746854', activity: 'hiking', radius: '19'});
 
   it('should return an object when calling the getplaces function', () => {
@@ -59,7 +63,23 @@ describe('test functions in the trailapi module', () => {
     return expect(getTestPlaces).to.eventually.have.property('places').to.be.an('array').to.have.lengthOf.above(0);
   });
   it('should return an object with an array of objects with a name property', () => {
-    // return expect(getTestPlaces).to.eventually.nested.include({'getTestPlaces.places' : 'name'});
+    return expect(getTestPlaces).to.eventually.have.nested.property("places[0].name");
+  });
+
+  //cleanDescription function
+
+  it('should change br tags, less than tags, and greater than tags to a blank space', () =>{
+    expect(trailapi.cleanDescription('<br />')).to.equal('');
+    expect(trailapi.cleanDescription('<br /><br />')).to.equal('');
+    expect(trailapi.cleanDescription('br /')).to.equal('');
+    expect(trailapi.cleanDescription('&lt;')).to.equal('');
+    expect(trailapi.cleanDescription('&gt;')).to.equal('');
+  });
+
+  it('should replace the space between a period and a word (or if there is no space between a period and a word) with unicode for 2 spaces', () => {
+    expect(trailapi.cleanDescription('.word')).to.equal('.\xa0\xa0word');
+    expect(trailapi.cleanDescription('. word')).to.equal('.\xa0\xa0word');
+    expect(trailapi.cleanDescription('.  word')).to.equal('.\xa0\xa0word');
   });
 
 });
@@ -69,6 +89,8 @@ describe('test functions in the trailapi module', () => {
 describe('test functions in the openweather api module', () => {
   let getTestWeather = weatherapi.getWeather('39.80001161','-80.22746854');
 
+  // getweather function
+
   it('should return an object when calling the getweather function', () => {
     return expect(getTestWeather).to.eventually.be.an('object');
   });
@@ -76,4 +98,49 @@ describe('test functions in the openweather api module', () => {
     expect(getTestWeather).to.eventually.have.property('main');
     return expect(getTestWeather).to.eventually.have.property('wind');
   });
+  it('should return an object with a property of main that has a property of temperature', () => {
+    return expect(getTestWeather).to.eventually.have.nested.property('main.temp');
+  });
+  it('should return an object with a property of wind that has a property of speed', () => {
+    return expect(getTestWeather).to.eventually.have.nested.property('wind.speed');
+  });
+
+  // convertToDirection function
+  it('should return the direction of ESE when you pass in 90', () => {
+    expect(weatherapi.convertToDirection(90)).to.equal('ESE');
+  });
+});
+
+// middleware test suite
+
+describe('test functions in the middleware module', () => {
+
+  let req1 = {};
+  let req2 = {};
+  let res = {};
+
+  beforeEach((done) => {
+    req1 = httpMocks.createRequest({ url: '/user' });
+    req2 = httpMocks.createRequest({ url: '/user' });
+    res = httpMocks.createResponse();
+    req1.session = {};
+    req2.session = {username: 'woof'};
+    done();
+  })
+
+  it('should not return an error message when you do pass in a session user name', (done) => {
+    mid.loginRequired(req2,res, function next(error) {
+      if (error) { throw new Error('login is requried'); }
+      else { console.log('you are logged in'); }
+      done();
+    });
+  });
+  it('should return an error message when you do not pass in a session user name', (done) => {
+    expect(mid.loginRequired(req1,res, function next(error) {
+      // if (error) { throw new Error('login is requried'); }
+      // else { console.log('you are logged in'); }
+      // done();
+    })).to.throw(Error('login is requried'));
+  });
+
 });
